0dc183347352f4e7c5554a1b0facdff6ed0b4f11
gio <gio@macmoro.com>
2019-04-05 22:44:50 -0600

    tested login

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    DB: 'mongodb://localhost:27017/auth'
+   // DB: 'mongodb://localhost:27017/auth'
+    DB:'mongodb+srv://admin-1:passwordadmin-1@web3-asg2-rk0iv.mongodb.net/test?retryWrites=true'
 }
